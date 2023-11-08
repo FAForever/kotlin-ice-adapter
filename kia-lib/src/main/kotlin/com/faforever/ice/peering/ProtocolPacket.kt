065994a322d29ad6a474b43f60d48d69f4b1e67a
Brutus5000 <Brutus5000@gmx.net>
2023-11-08 23:51:46 +0100

    Fix cutting last byte

--- kia-lib/src/main/kotlin/com/faforever/ice/peering/ProtocolPacket.kt
+++ kia-lib/src/main/kotlin/com/faforever/ice/peering/ProtocolPacket.kt
@@ -13,7 +13,8 @@ data class GameDataPacket(val data: ByteArray) : ProtocolPacket {
     companion object {
         const val PREFIX = 'd'.code.toByte()
 
-        fun fromWire(wireData: ByteArray) = GameDataPacket(wireData.copyOfRange(1, wireData.lastIndex))
+        // copyOfRange toIndex is EXCLUSIVE! Thus, we don't use .lastIndex, but .size
+        fun fromWire(wireData: ByteArray) = GameDataPacket(wireData.copyOfRange(fromIndex = 1, toIndex = wireData.size))
     }
 
     override val prefix = PREFIX
