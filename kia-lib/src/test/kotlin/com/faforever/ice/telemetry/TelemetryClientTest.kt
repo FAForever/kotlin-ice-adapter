0d16e1e5b102f07bf54095831de06669045579f7
Ryan Hieber <floatpt0@gmail.com>
2024-03-26 02:03:09 -0700

    Fix tests failing because UUID wasn't unmocked and reorganize

--- kia-lib/src/test/kotlin/com/faforever/ice/telemetry/TelemetryClientTest.kt
+++ kia-lib/src/test/kotlin/com/faforever/ice/telemetry/TelemetryClientTest.kt
@@ -7,7 +7,9 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockkConstructor
 import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
 import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -16,33 +18,39 @@ import java.util.UUID
 
 @ExtendWith(MockKExtension::class)
 class TelemetryClientTest {
+    private lateinit var sut: TelemetryClient
+
     @MockK
     private lateinit var mockIceOptions: IceOptions
+
+    private val messageUuid = UUID.fromString("000e8400-e29b-41d4-a716-446655440000")
 
     @BeforeEach
     fun beforeEach() {
         every { mockIceOptions.telemetryServer } returns "wss://mock-telemetryserver.xyz:"
         every { mockIceOptions.userId } returns 5000
         every { mockIceOptions.gameId } returns 12345
+        mockkStatic(UUID::class)
+        every { UUID.randomUUID() } returns messageUuid
 
         mockkConstructor(TelemetryClient.TelemetryWebsocketClient::class)
         every { anyConstructed<TelemetryClient.TelemetryWebsocketClient>().connect() } returns Unit
+        sut = TelemetryClient(mockIceOptions, jacksonObjectMapper())
+    }
+
+    @AfterEach
+    fun afterEach() {
+        unmockkStatic(UUID::class)
     }
 
     @Test
     fun `test update coturn servers`() {
-        val messageUuid = UUID.fromString("000e8400-e29b-41d4-a716-446655440000")
-        mockkStatic(UUID::class)
-        every { UUID.randomUUID() } returns messageUuid
-
-        val telemetryClient = TelemetryClient(mockIceOptions, jacksonObjectMapper())
-
         val coturnServers: List<com.faforever.ice.peering.CoturnServer> = listOf(
             com.faforever.ice.peering.CoturnServer(URI.create("stun://coturn1.faforever.com:3478")),
             com.faforever.ice.peering.CoturnServer(URI.create("stun://fr-turn1.xirsys.com:80")),
         )
 
-        telemetryClient.updateCoturnList(coturnServers)
+        sut.updateCoturnList(coturnServers)
 
         verify {
             val expected = "{\"messageType\":\"UpdateCoturnList\"," +
@@ -57,14 +65,9 @@ class TelemetryClientTest {
 
     @Test
     fun `test update coturn servers empty`() {
-        val messageUuid = UUID.fromString("000e8400-e29b-41d4-a716-446655440000")
-        mockkStatic(UUID::class)
-        every { UUID.randomUUID() } returns messageUuid
-
-        val telemetryClient = TelemetryClient(mockIceOptions, jacksonObjectMapper())
         val coturnServers: List<com.faforever.ice.peering.CoturnServer> = listOf()
 
-        telemetryClient.updateCoturnList(coturnServers)
+        sut.updateCoturnList(coturnServers)
 
         verify {
             val expected = "{\"messageType\":\"UpdateCoturnList\"," +
